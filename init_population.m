bde81131234c245a13de077884508136f7cfd846
WKCN <wkcn@live.cn>
2015-08-24 20:02:32 +0800

    first commit

---
+++ init_population.m
@@ -0,0 +1,14 @@
+function X = init_population(bound,m)
+    %m is the number of population
+    %n is the number of feature
+    %bound is a 2 x n matrix
+    %return X,X is the all of population
+    n = size(bound,2);
+    X = zeros(m,n);
+    delta = bound(2,:) - bound(1,:);
+    parfor i = 1:m
+        X(i,:) = rand(1,n) .* delta + bound(1,:);
+    end
+end
+    
+    
